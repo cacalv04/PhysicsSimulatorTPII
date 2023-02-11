39f3d6f30fe11ca5ab0e487648d8e3e7f3ab8ef9
34657 <34657@DESKTOP-E86JV8V>
2023-02-11 19:27:17 +0100

    atributos y constructora de la clase body

--- src/simulator/model/Body.java
+++ src/simulator/model/Body.java
@@ -1,5 +1,16 @@
 package simulator.model;
 
+import simulator.misc.Vector2D;
+
 public abstract class Body {
 
+	protected String ID;
+	protected String GID;
+	protected Vector2D speed;
+	protected Vector2D force;
+	protected Vector2D pos;
+	protected double mass;
+	
+	
+	
 }
