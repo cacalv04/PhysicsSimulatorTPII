039599880e8ec17c08d536526409ee6de7870f4d
34657 <34657@DESKTOP-E86JV8V>
2023-02-11 19:30:45 +0100

    validArguments function

--- src/simulator/model/Body.java
+++ src/simulator/model/Body.java
@@ -11,6 +11,26 @@ public abstract class Body {
 	protected Vector2D pos;
 	protected double mass;
 	
-	
+	private Body(String ID, String GID, Vector2D speed, Vector2D pos, double mass) {
+		try {
+			validArguements(ID, GID, speed, pos, mass);
+			this.ID = ID;
+			this.GID = GID;
+			this.speed = speed;
+			this.pos = pos;
+			this.mass = mass;
+			this.force = new Vector2D(0, 0);
+		}
+		catch (IllegalArgumentException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+		
+	}
+
+	private void validArguements(String iD2, String gID2, Vector2D speed2, Vector2D pos2, double mass2) {
+		// TODO Auto-generated method stub
+		if(iD2 == null|| gID2 == null || speed2 == null || pos2 == null || mass2 < 0 ) throw new IllegalArgumentException("[ERROR]: an argument value is null or negative");
+		if (iD2.trim().length()>0 || gID2.trim().length()>0) throw new IllegalArgumentException("[ERROR]: id or gid not including a character not void");
+	}
 	
 }
