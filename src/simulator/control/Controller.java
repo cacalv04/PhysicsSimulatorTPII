20c05020f99c9a6c52017f0086b13e8073abc027
cacalv04 <cacalv04@ucm.es>
2023-02-26 02:23:43 +0100

    controller

--- src/simulator/control/Controller.java
+++ src/simulator/control/Controller.java
@@ -2,6 +2,7 @@ package simulator.control;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -29,10 +30,15 @@ public class Controller {
 			JSONObject jsonInupt = new JSONObject(new JSONTokener(in));   //jsonInupt contiene lo que hay en todo el fichero in
 			
 			JSONArray groups = jsonInupt.getJSONArray("groups");    //groups es un array de todos los grupos que hay en formato JSON
+			JSONArray bodies = jsonInupt.getJSONArray("bodies");
 			
 			for(int i = 0; i < groups.length(); i++) {
-				//physicsSimulator.addGroup(bodiesFactory.createInstance(groups.getJSONObject(i)));
+				physicsSimulator.addGroup(groups.getString(i));
 			}
+			for(int i = 0; i < groups.length(); i++) {
+				physicsSimulator.addBody(bodiesFactory.createInstance(bodies.getJSONObject(i)));
+			}
+			//puede que falte la clave laws
 		}
 		catch (IllegalArgumentException ex) {
 			throw ex;
@@ -40,6 +46,16 @@ public class Controller {
 	}
 	
 	public void run(int n, OutputStream out) {
+		PrintStream p = new PrintStream(out);
+		p.println("{");
+		p.println("\"states\": [");
 		
+		p.println(physicsSimulator.getState());
+		
+		for(int i = 1; i <= n; i++) {
+			p.println(physicsSimulator.getState());
+		}
+		p.println("]");
+		p.println("}");
 	}
 }
