8ba23b7bf874939a3c8ba9c3418ef8f67683d0d9
cacalv04 <cacalv04@ucm.es>
2023-03-18 18:37:26 +0100

    tablas

---
+++ src/simulator/view/InfoTable.java
@@ -0,0 +1,28 @@
+package simulator.view;
+
+import javax.swing.JPanel;
+import javax.swing.table.TableModel;
+
+public class InfoTable extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	String _title;
+	TableModel _tableModel;
+	
+	InfoTable (String title, TableModel tableModel){
+		_title = title;
+		_tableModel = tableModel;
+		initGUI();
+	}
+	
+	private void initGUI() {
+		// TODO cambiar el layout del panel a BorderLayout()
+		// TODO añadir un borde con título al JPanel, con el texto _title
+		// TODO añadir un JTable (con barra de desplazamiento vertical) que use
+		// _tableModel
+	}
+}
---
+++ src/simulator/view/MainWindow.java
@@ -0,0 +1,63 @@
+package simulator.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+import simulator.control.Controller;
+
+public class MainWindow extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Controller _ctrl;
+	
+	public MainWindow(Controller ctrl) {
+		super("Physics Simulator");
+		_ctrl = ctrl;
+		initGUI();
+	}
+
+	private void initGUI() {
+		
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		setContentPane(mainPanel);
+		
+		// TODO crear ControlPanel y añadirlo en PAGE_START de mainPanel
+		JPanel ControlPanel = new JPanel();
+		mainPanel.add(ControlPanel, BorderLayout.PAGE_START);
+		
+		// TODO crear StatusBar y añadirlo en PAGE_END de mainPanel
+		JPanel StatusBar = new JPanel();
+		mainPanel.add(StatusBar, BorderLayout.PAGE_END);
+		
+		// Definición del panel de tablas (usa un BoxLayout vertical)
+		JPanel contentPanel = new JPanel();
+		contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+		mainPanel.add(contentPanel, BorderLayout.CENTER);
+		
+		// TODO crear la tabla de grupos y añadirla a contentPanel.
+		// Usa setPreferredSize(new Dimension(500, 250)) para fijar su tamaño
+		InfoTable groupTable = new InfoTable("Groups", new GroupsTableModel(_ctrl));
+		groupTable.setPreferredSize(new Dimension(500, 250));
+		contentPanel.add(groupTable);
+		
+		// TODO crear la tabla de cuerpos y añadirla a contentPanel.
+		// Usa setPreferredSize(new Dimension(500, 250)) para fijar su tamaño
+		InfoTable bodiesTable = new InfoTable("Bodies", new BodiesTableModel(_ctrl));
+		bodiesTable.setPreferredSize(new Dimension(500, 200));
+		contentPanel.add(bodiesTable);
+		
+		// TODO llama a Utils.quit(MainWindow.this) en el método windowClosing
+		addWindowListener( … );
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		pack();
+		setVisible(true);
+	}
+}
